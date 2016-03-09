abf426e38f5a5f88668ce2504b0fdfcc30dacff7
Nitish Sharma <nitish.sharma@keepworks.com>
2016-03-09 18:56:51 +0530

    Minor change

--- app/components/filter-listing.js
+++ app/components/filter-listing.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   filter: null,
-  filteredList: null,
   actions: {
     autoComplete() {
       this.get('autoComplete')(this.get('filter'));
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  filteredList: null,
   actions: {
     autoComplete(param) {
       if(param !== "") {
